4a32a4c3047383b2fc5aa1e463dad7616a5c3fd7
Bruno Oliveira <bruoli3@gmail.com>
2023-09-19 09:20:48 -0300

    feat: session class based on sgbd

--- app/core/database/db.py
+++ app/core/database/db.py
@@ -11,35 +11,23 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import get_config
 
 
-config = get_config()
-engine: AsyncEngine = create_async_engine(config.MYSQL_DATABASE_URI, echo=True)
-Session = sessionmaker(
-    bind=engine, class_=AsyncSession, expire_on_commit=False
-)
-
-
-async def get_async_session() -> AsyncGenerator[AsyncSession, Any]:
-    async with Session() as session:
-        yield session
-
-
 class Session:
     def __init__(self, driver: str) -> None:
         self.driver = driver
         self._config = get_config()
-        self._engine: Optional[AsyncEngine] = self.get_uri()
+        self._engine: Optional[AsyncEngine] = self.get_engine()
         self._session = sessionmaker(
-            bind=engine, class_=AsyncSession, expire_on_commit=False
+            bind=self._engine, class_=AsyncSession, expire_on_commit=False
         )
 
-    def get_uri(self) -> str:
+    def get_engine(self) -> str:
         if self.driver == 'mysql':
             return create_async_engine(
-                config.MYSQL_DATABASE_URI, echo=True
+                self._config.MYSQL_DATABASE_URI, echo=True
             )
         elif self.driver == 'postgres':
             return create_async_engine(
-                config.POSTGRES_DATABASE_URI, echo=True
+                self._config.POSTGRES_DATABASE_URI, echo=True
             )
         else:
             raise Exception("Driver not found")

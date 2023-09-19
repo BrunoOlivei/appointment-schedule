9838d561c6787c112e204a7d1817dd8db1805414
Bruno Oliveira <bruoli3@gmail.com>
2023-09-19 09:25:01 -0300

    feat: users routes

--- app/users/api/V1/__init__.py
+++ app/users/api/V1/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from .permissions import router as permissions_router
+from .users import router as users_router
 from app.core.config import get_config
 
 
@@ -10,3 +11,4 @@ router = APIRouter(prefix=config.API_PREFIX_V1)
 
 
 router.include_router(permissions_router)
+router.include_router(users_router)
